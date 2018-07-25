c81db53435df4be599470bc6a90f63c0e8316cc0
Dan Bock <danbock@gmail.com>
2018-07-24 21:36:06 -0400

    Fix MySQL error

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -6,7 +6,8 @@
 
 $ok_array['data'] = pdo_select('
   select
-    r.week_id - 1 as week, /* week IDs are zero-based from now on */
+    /* http://nikolaynaychov.blogspot.com/2012/08/how-to-fix-mysql-error-bigint-unsigned.html */
+    cast(r.week_id as signed) - 1 as week, /* week IDs are zero-based from now on */
     r.weight,
     r.aerobic_minutes,
     r.strength_minutes,
@@ -15,7 +16,9 @@ $ok_array['data'] = pdo_select('
     reports_with_fitbit_hybrid r
     inner join current_classes c
       on r.class_id = c.class_id
-  where r.user_id = ?
+  where
+    r.user_id = ?
+    and r.week_id > 0
 ', $ok_array['userId']);
 
 $ok_array['class'] = current_class_by_user($ok_array['userId']);
